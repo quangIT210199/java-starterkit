86296fecf2ec9e66e0112395feed24cfa8222361
quang <quang.itptit@gmail.com>
2021-04-20 14:03:58 +0700

    first commit

---
+++ src/main/java/cls/codelover/javastarterkit/JavaStarterkitApplication.java
@@ -0,0 +1,13 @@
+package cls.codelover.javastarterkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStarterkitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStarterkitApplication.class, args);
+    }
+
+}
